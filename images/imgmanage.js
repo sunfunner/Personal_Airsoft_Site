ac6b17199c2f23fede5495b6d15e9c31dcb62c69
Rollipop <aonfunner@hotmail.com>
2025-10-23 08:57:02 +0700

    Add files via upload

---
+++ images/imgmanage.js
@@ -0,0 +1,296 @@
+// ==== ไฟล์ imgmanage.js ====
+const galleryNoImg = [
+  {
+    src: "images/weapon/full/N-1.jpg",
+  }
+];
+
+const gallerym50 = [
+  {
+    src: "images/weapon/full/m50-1.jpg",
+  },
+  {
+    src: "images/weapon/full/m50-2.jpg",
+  },
+  {
+    src: "images/weapon/full/m50-3.jpg",
+  },
+  {
+    src: "images/weapon/full/m50-4.jpg",
+  },
+  {
+    src: "images/weapon/full/m50-5.jpg",
+  },
+  {
+    src: "images/weapon/full/m50-6.jpg",
+  },
+  {
+    src: "images/weapon/full/m50-7.jpg",
+  }
+];
+
+
+const gallerym27 = [
+  {
+    src: "images/weapon/full/m27-1.jpg",
+  },
+  {
+    src: "images/weapon/full/m27-2.jpg",
+  },
+  {
+    src: "images/weapon/full/m27-3.jpg",
+  },
+  {
+    src: "images/weapon/full/m27-4.jpg",
+  },
+  {
+    src: "images/weapon/full/m27-5.jpg",
+  }
+];
+
+const gallerySCARL = [
+  {
+    src: "images/weapon/full/scar-l-1.jpg",
+  },
+  {
+    src: "images/weapon/full/scar-l-2.jpg",
+  },
+  {
+    src: "images/weapon/full/scar-l-3.jpg",
+  },
+  {
+    src: "images/weapon/full/scar-l-4.jpg",
+  },
+  {
+    src: "images/weapon/full/scar-l-5.jpg",
+  }
+];
+
+
+const galleryMP9 = [
+  {
+    src: "images/weapon/full/mp9-1.jpg",
+  },
+  {
+    src: "images/weapon/full/mp9-2.jpg",
+  },
+  {
+    src: "images/weapon/full/mp9-3.jpg",
+  },
+  {
+    src: "images/weapon/full/mp9-4.jpg",
+  },
+  {
+    src: "images/weapon/full/mp9-5.jpg",
+  },
+  {
+    src: "images/weapon/full/mp9-6.jpg",
+  },
+  {
+    src: "images/weapon/full/mp9-7.jpg",
+  },
+  {
+    src: "images/weapon/full/mp9-8.jpg",
+  }
+];
+
+const galleryDEl6 = [
+  {
+    src: "images/weapon/full/del6-1.jpg",
+  },
+  {
+    src: "images/weapon/full/del6-2.jpg",
+  },
+  {
+    src: "images/weapon/full/del6-3.jpg",
+  },
+  {
+    src: "images/weapon/full/del6-4.jpg",
+  },
+  {
+    src: "images/weapon/full/del6-5.jpg",
+  },
+  {
+    src: "images/weapon/full/del6-6.jpg",
+  },
+  {
+    src: "images/weapon/full/del6-7.jpg",
+  },
+  {
+    src: "images/weapon/full/del6-8.jpg",
+  },
+  {
+    src: "images/weapon/full/del6-9.jpg",
+  },
+  {
+    src: "images/weapon/full/del6-10.jpg",
+  }
+];
+
+const galleryDE = [
+  {
+    src: "images/weapon/full/de-1.jpg",
+  },
+  {
+    src: "images/weapon/full/de-2.jpg",
+  },
+  {
+    src: "images/weapon/full/de-3.jpg",
+  },
+  {
+    src: "images/weapon/full/de-4.jpg",
+  },
+  {
+    src: "images/weapon/full/de-5.jpg",
+  },
+  {
+    src: "images/weapon/full/de-6.jpg",
+  },
+  {
+    src: "images/weapon/full/de-7.jpg",
+  },
+  {
+    src: "images/weapon/full/de-8.jpg",
+  }
+];
+
+const galleryM1911 = [
+  {
+    src: "images/weapon/full/m1911-1.jpg",
+  },                         
+  {                          
+    src: "images/weapon/full/m1911-2.jpg",
+  },                         
+  {                          
+    src: "images/weapon/full/m1911-3.jpg",
+  },                         
+  {                          
+    src: "images/weapon/full/m1911-4.jpg",
+  },                         
+  {                          
+    src: "images/weapon/full/m1911-5.jpg",
+  },                         
+  {                          
+    src: "images/weapon/full/m1911-6.jpg",
+  },                         
+  {                          
+    src: "images/weapon/full/m1911-7.jpg",
+  },                         
+  {                          
+    src: "images/weapon/full/m1911-8.jpg",
+  },                         
+  {                          
+    src: "images/weapon/full/m1911-9.jpg",
+  }
+];
+
+const galleryPyton = [
+  {
+    src: "images/weapon/full/pyton-1.jpg",
+  },                         
+  {                          
+    src: "images/weapon/full/pyton-2.jpg",
+  },                         
+  {                          
+    src: "images/weapon/full/pyton-3.jpg",
+  },                         
+  {                          
+    src: "images/weapon/full/pyton-4.jpg",
+  },                         
+  {                          
+    src: "images/weapon/full/pyton-5.jpg",
+  },                         
+  {                          
+    src: "images/weapon/full/pyton-6.jpg",
+  },                         
+  {                          
+    src: "images/weapon/full/pyton-7.jpg",
+  }
+];
+
+const galleryBigrrr = [
+  {
+    src: "images/weapon/full/bigrrr-1.jpg",
+  }
+];
+
+const galleryM67 = [
+  {
+    src: "images/weapon/full/m67-1.jpg",
+  }
+];
+
+const galleryC4 = [
+  {
+    src: "images/weapon/full/c4-1.jpg",
+  }
+];
+
+const galleryM84 = [
+  {
+    src: "images/weapon/full/m84-1.jpg",
+  },
+  {
+    src: "images/weapon/full/m84-2.jpg",
+  },
+  {
+    src: "images/weapon/full/m84-3.jpg",
+  },
+  {
+    src: "images/weapon/full/m84-4.jpg",
+  },
+  {
+    src: "images/weapon/full/m84-5.jpg",
+  },
+  {
+    src: "images/weapon/full/m84-6.jpg",
+  },
+  {
+    src: "images/weapon/full/m84-7.jpg",
+  }
+];
+
+const gallerySpareM1911 = [
+  {
+    src: "images/weapon/full/spa1911-1.jpg",
+  },
+  {
+    src: "images/weapon/full/spa1911-2.jpg",
+  },
+  {
+    src: "images/weapon/full/spa1911-3.jpg",
+  },
+  {
+    src: "images/weapon/full/spa1911-4.jpg",
+  },
+  {
+    src: "images/weapon/full/spa1911-5.jpg",
+  },
+  {
+    src: "images/weapon/full/spa1911-6.jpg",
+  },
+  {
+    src: "images/weapon/full/spa1911-7.jpg",
+  },
+  {
+    src: "images/weapon/full/spa1911-8.jpg",
+  }
+];
+
+// รวมชุดภาพทั้งหมด
+const galleryCollections = {
+	galsn: galleryNoImg,
+	galm50: gallerym50,
+	galm27: gallerym27,
+	galscarl: gallerySCARL,
+	galmp9: galleryMP9,
+	galdel6: galleryDEl6,
+	galde: galleryDE,
+	galm1911: galleryM1911,
+	galpyton: galleryPyton,
+	galbigrrr: galleryBigrrr,
+	galm67: galleryM67,
+	galc4: galleryC4,
+	galm84: galleryM84,
+	galspare1911: gallerySpareM1911
+};
