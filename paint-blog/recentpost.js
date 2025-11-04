8eb37a9d1e54e0b802c461a7721904f10514e049
Rollipop <aonfunner@hotmail.com>
2025-11-04 17:19:31 +0700

    Add files via upload

--- paint-blog/recentpost.js
+++ paint-blog/recentpost.js
@@ -5,23 +5,23 @@ document.addEventListener("DOMContentLoaded", function() {
 		<h5 class="font-secondary mb-4"><i class="ti-minus mr-2 text-color"></i>Recent posts</h5>
 		
 		<div class="media mb-4">
-			<img src="images/blog/blog-post-5.jpg" alt="" class="img-fluid pr-4 w-50 align-self-center">
+			<img src="../images/paint/bullet-paint-1.png" alt="" class="img-fluid pr-4 w-50 align-self-center">
 			<div class="media-body">
-				<a href="#" class="text-black d-block lh-25"> Track your daily body fitness</a>
+				<a href="#" class="text-black d-block lh-25">5.56mm and 9mm Bullet Paint</a>
 			</div>
 		</div>
 		
 		<div class="media mb-4">
-			<img src="images/blog/blog-post-6.jpg" alt="" class="img-fluid pr-4 w-50 align-self-center">
+			<img src="../images/paint/bullet-clean-1.png" alt="" class="img-fluid pr-4 w-50 align-self-center">
 			<div class="media-body">
-				<a href="#" class="text-black d-block lh-25">Keep your body fitness track</a>
+				<a href="#" class="text-black d-block lh-25">Bullet Clean-up</a>
 			</div>
 		</div>
 		
 		<div class="media mb-4">
-			<img src="images/blog/post1.jpg" alt="" class="img-fluid pr-4 w-50 align-self-center">
+			<img src="../images/paint/pyton-modyfy-1.png" alt="" class="img-fluid pr-4 w-50 align-self-center">
 			<div class="media-body">
-				<a href="#" class="text-black d-block lh-25">Keep your body fitness track</a>
+				<a href="#" class="text-black d-block lh-25">Pyton Cylinder & Bullet Fix</a>
 			</div>
 		</div>
 	</div>
